5282c66b9093b4bed5106f5a84d449f92d16eadf
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-10-21 12:57:10 +0100

    Configuration de la première couche

---
+++ service-proxy/src/test/java/com/banking_system/service_proxy/ServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking_system.service_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service-registry/src/test/java/com/banking_system/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking_system.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
