25d93a3264a0c07083467773bde521c80c2792b0
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-10-28 19:57:07 +0100

    Ajout du service de transactions

---
+++ service-transactions/src/test/java/com/banking_system/service_transactions/ServiceTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking_system.service_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
