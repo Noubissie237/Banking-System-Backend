034724162ee24f8d1ab6be9680043f8d85002acc
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-11-02 12:09:10 +0100

    Service de transactions

--- service-transactions/src/main/java/com/banking_system/service_transactions/models/StatutTransaction.java
+++
@@ -1,7 +0,0 @@
-package com.banking_system.service_transactions.models;
-
-public enum StatutTransaction {
-    EN_ATTENTE,
-    ACCEPTEE,
-    REJETEE
-}
--- service-transactions/src/main/java/com/banking_system/service_transactions/models/TypeTransaction.java
+++
@@ -1,7 +0,0 @@
-package com.banking_system.service_transactions.models;
-
-public enum TypeTransaction {
-    DEPOT,
-    RETRAIT,
-    TRANSFERT
-}
