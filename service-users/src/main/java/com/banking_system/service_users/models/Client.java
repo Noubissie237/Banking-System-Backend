5a5af5e4fd72c42f3647ca460f635dbc586e1bd4
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-11-10 09:33:51 +0100

    Ajout des roles

--- service-users/src/main/java/com/banking_system/service_users/models/Client.java
+++ service-users/src/main/java/com/banking_system/service_users/models/Client.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @Setter
 public class Client extends Person {
 
+    private Role role = Role.CLIENT;
     public Client(String nom, String prenom, String email, String tel, String numero_cni, String recto_cni, String verso_cni, String password) {
         super(nom, prenom, email, tel, numero_cni, recto_cni, verso_cni, password);
     }
