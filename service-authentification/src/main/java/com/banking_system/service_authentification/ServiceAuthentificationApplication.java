850852ee2ed9957af9f118ae6e9f98e67890476a
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-11-10 08:35:39 +0100

    Initial Configurations

--- service-authentification/src/main/java/com/banking_system/service_authentification/ServiceAuthentificationApplication.java
+++ service-authentification/src/main/java/com/banking_system/service_authentification/ServiceAuthentificationApplication.java
@@ -2,7 +2,9 @@ package com.banking_system.service_authentification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceAuthentificationApplication {
 
