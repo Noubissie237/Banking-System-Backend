30612f1548265e9149ebdd79012ae209a4108407
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-11-16 03:46:26 +0100

    Token de 05 min

--- service-authentification/src/main/java/com/banking_system/service_authentification/utils/Utils.java
+++ service-authentification/src/main/java/com/banking_system/service_authentification/utils/Utils.java
@@ -22,7 +22,7 @@ public class Utils {
         SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
 
         Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + 86400000);
+        Date expiryDate = new Date(now.getTime() + 300000);
 
         JwtBuilder builder = Jwts.builder()
             .setSubject(user.getTel())
