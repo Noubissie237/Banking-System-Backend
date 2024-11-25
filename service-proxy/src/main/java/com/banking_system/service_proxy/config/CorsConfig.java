811122ac9e3d10a0af72ab6a7d914766e277025c
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-11-25 14:21:12 +0100

    For deploy

--- service-proxy/src/main/java/com/banking_system/service_proxy/config/CorsConfig.java
+++ service-proxy/src/main/java/com/banking_system/service_proxy/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.banking_system.service_proxy;
+package com.banking_system.service_proxy.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
